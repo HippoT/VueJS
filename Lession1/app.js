8758c2519c631e4e9deb169414d1f8ce2891c80e
HippoT <minhtuan.02071995@gmail.com>
2018-07-05 14:08:53 +0700

    learning somthing cool

--- Lession1/app.js
+++ Lession1/app.js
@@ -1,3 +1,39 @@
+
+
+Vue.component('blog-post', {
+    props: ['post'],
+    template: `
+      <div class="blog-post">
+        <h3>{{ post.title }}</h3>
+        <button v-on:click="$emit('enlarge-text',post.id)">
+          Enlarge text
+        </button>
+        <div v-html="post.content"></div>
+      </div>
+    `
+});
+new Vue({
+    el: '#blog-posts-events-demo',
+    data: {
+        posts: [
+            { id: 1, title: 'My journey with Vue', fontSize: 1 },
+            { id: 2, title: 'Blogging with Vue', fontSize: 1 },
+            { id: 3, title: 'Why Vue is so fun', fontSize: 1 }
+        ],
+        postFontSize: 1
+    },
+    methods:{
+        onEnlargeText: function (id) {
+            for(var i = 0; i < this.posts.length; i++){
+                if(this.posts[i].id == id){
+                    this.posts[i].fontSize += 0.1;
+                }
+            }
+          }
+    }
+});
+
+
 // new Vue({
 //     el: '#vue-app',
 //     data: {
@@ -27,22 +63,22 @@
 //     }
 // });
 
-new Vue({
-    el: "#vue-app",
-    data: {
-        health: 100,
-        ended: false
-    },
-    methods:{
-        punchClick: function(){
-            this.health -= 10;
-            if(this.health <= 0){
-                this.ended = true;
-            }
-        },
-        restartClick: function(){
-            this.ended = false;
-            this.health = 100;
-        }
-    }
-});
+// new Vue({
+//     el: "#vue-app",
+//     data: {
+//         health: 100,
+//         ended: false
+//     },
+//     methods:{
+//         punchClick: function(){
+//             this.health -= 10;
+//             if(this.health <= 0){
+//                 this.ended = true;
+//             }
+//         },
+//         restartClick: function(){
+//             this.ended = false;
+//             this.health = 100;
+//         }
+//     }
+// });
